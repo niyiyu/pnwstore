a5a559f6d8e41d64ce400d3c857fae4425b3f7e7
Yiyu Ni <niyiyu@uw.edu>
2022-08-08 14:38:00 -0700

    auxiliary scripts

---
+++ scripts/mpi_index_mseed.py
@@ -0,0 +1,19 @@
+from mpi4py import MPI
+import glob
+import os
+
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+mseedindex_cmd="mseedindex"
+path = "/data/wsd01/HOOD_data"
+sqlite_path = "/data/wsd01/HOOD_data/mseed.sqlite"
+leap_env = "LIBMSEED_LEAPSECOND_FILE=../leap-seconds.list"
+
+for idx, i in enumerate(glob.glob(path + "/**/*", recursive=True)):
+    if idx % size == rank:
+        if not os.path.isdir(i):
+                cmd = " ".join([leap_env, mseedindex_cmd, i, "-sqlite", sqlite_path])
+                os.system(cmd)
